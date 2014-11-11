a771a0658b19197fbc95542fdd1e8d192e2c8813
phlppchrtn <pchretien55@gmail.com>
2014-11-11 21:44:07 +0100

    test

--- angular/helloworld/controller.js
+++ angular/helloworld/controller.js
@@ -1,3 +1,17 @@
-function HelloCntl($scope) {
-  $scope.name = 'World';
-}
+var myApp = angular.module('myApp', [])
+	.controller('HelloCtrl', ['$scope', function ($scope) {
+		$scope.name = 'World';
+		$scope.firstname = 'roger';
+	}]);
+
+myApp.directive('xinput', function () {
+	return {
+	    //restrict: 'A',
+	    replace: true,
+	    //transclude: true,
+	    template: '<input type="text" ng-model="name" />'
+/*	    link: function (scope, element, attrs) {
+	      // DOM manipulation/events here!
+	    }
+*/	  };
+});
