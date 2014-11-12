a7aabc20a94d3059fea9db5abc8c05c3555d53d9
phlppchrtn <pchretien55@gmail.com>
2014-11-12 23:14:12 +0100

    directives

--- angular/helloworld/controller.js
+++ angular/helloworld/controller.js
@@ -2,16 +2,29 @@ var myApp = angular.module('myApp', [])
 	.controller('HelloCtrl', ['$scope', function ($scope) {
 		$scope.name = 'World';
 		$scope.firstname = 'roger';
+		$scope.users = [
+			{"first":"jean", "last":"dupond", dt:"DT_USER"}, 
+			{"first":"paul", "last":"rapon", dt:"DT_USER"}
+		];
+		$scope.dtdefinition={"DT_USER":[{"first":"DO_S"}, {"last":"DO_S"}]};
+		$scope.domain={ "DO_S" :{"datatype":"string"}};
 	}]);
 
-myApp.directive('xinput', function () {
-	return {
-	    //restrict: 'A',
-	    replace: true,
-	    //transclude: true,
-	    template: '<input type="text" ng-model="name" />'
-/*	    link: function (scope, element, attrs) {
-	      // DOM manipulation/events here!
-	    }
-*/	  };
-});
+myApp
+	.directive('xinput', function () {
+		return {
+		    restrict: 'E',
+		    replace: true,
+		    template: '<input type="text" ng-model="name"/>'
+		  };
+	})
+	.directive('zzfield', function () {
+		return {
+			scope: {
+      			dto: '=dto'
+    		},
+		    restrict: 'EA',
+		    replace: true,
+		    template: '<div>{{dto.first}} -- {{dto.last}}</div>'
+		  };
+	});
