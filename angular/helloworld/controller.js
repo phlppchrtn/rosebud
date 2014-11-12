5b05ac4753bb651517902fa3c3d9360df5097ce9
phlppchrtn <pchretien55@gmail.com>
2014-11-12 23:17:16 +0100

    dir

--- angular/helloworld/controller.js
+++ angular/helloworld/controller.js
@@ -2,6 +2,7 @@ var myApp = angular.module('myApp', [])
 	.controller('HelloCtrl', ['$scope', function ($scope) {
 		$scope.name = 'World';
 		$scope.firstname = 'roger';
+		$scope.movie = {"title":"shinning", "author":"kubrick"};
 		$scope.users = [
 			{"first":"jean", "last":"dupond", dt:"DT_USER"}, 
 			{"first":"paul", "last":"rapon", dt:"DT_USER"}
@@ -18,7 +19,7 @@ myApp
 		    template: '<input type="text" ng-model="name"/>'
 		  };
 	})
-	.directive('zzfield', function () {
+	.directive('zzdto', function () {
 		return {
 			scope: {
       			dto: '=dto'
